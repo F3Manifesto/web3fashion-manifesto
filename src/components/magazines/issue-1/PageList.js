791dabc61b2ac93f93fa9438c66aaa6197cc966c
digitalax_ <72898465+digitalax1@users.noreply.github.com>
2022-03-15 07:32:02 -0400

    pages

--- src/components/magazines/issue-1/PageList.js
+++ src/components/magazines/issue-1/PageList.js
@@ -5,8 +5,8 @@ const Page12 = loadable(() => import('./Page12'))
 const Page34 = loadable(() => import('./Page34'))
 const Page56 = loadable(() => import('./Page56'))
 const Page78 = loadable(() => import('./Page78'))
-/*const Page910 = loadable(() => import('./Page910'))
-const Page1112 = loadable(() => import('./Page1112'))
+const Page910 = loadable(() => import('./Page910'))
+/*const Page1112 = loadable(() => import('./Page1112'))
 const Page1314 = loadable(() => import('./Page1314'))
 const Page1516 = loadable(() => import('./Page1516'))*/
 
@@ -18,8 +18,8 @@ const PageList = [
   <Page34 />,
   <Page56 />,
   <Page78 />,
-  /*<Page910 />,
-  <Page1112 />,
+  <Page910 />,
+  /*<Page1112 />,
   <Page1314 />,
   <Page1516 />,*/
   
