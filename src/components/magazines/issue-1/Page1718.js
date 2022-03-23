403e3b577d398c118a78ccc64a05307e17d304a6
James Lin <wonyy1988@outlook.com>
2022-03-23 09:28:38 -0400

    fix: type animation

--- src/components/magazines/issue-1/Page1718.js
+++ src/components/magazines/issue-1/Page1718.js
@@ -9,33 +9,32 @@ import {
 
 
 const Page1718 = () => {
-return (
-  <div className={styles.wrapper}>
-  <p className={styles.one}>Dear code breaker,</p>
-  <p className={styles.two}>Welcome to the ZK cypherpunk roll up.</p>
-<br></br><br></br>
-<p className={styles.three}>This note follows on from earlier discussions on the radical restructuring of self sovereignty and Asymmetric Guerilla Intelligence (AGI).</p> 
-<br></br><br></br>
-<p>The Secret sharing spelled out below mixes methods from a distribution of decoys and tricks with a global call sign of д13а.</p>
-<br></br><br></br>
-<p>It forms unstoppable interconnected nodes of mathematical armaments against tyranny and privacy securing cryptographic defenses against the rise of the computer state. A state that cannot endure in this epidemic of disorientation.</p>
-<br></br><br></br>
-<p>To prepare yourself for the world that is coming you must first move beyond conventional thinking. The prevalence of Stockholm systems have lost touch with consensus reality and only continue to censor the free flow of information and sabotage the most useful of decentralised technologies that we have at our disposal.</p> 
-<br></br><br></br>
-<p>The rise of portable sovereignty requires cryptography, a cypherpunk ethos and unorthodox analysis.</p> 
-<br></br><br></br>
-<p>This mixture strengthens your ability to counter the institutions, the cartels and the devastating misuse of concentrated power we work to transcend.</p>
-<br></br><br></br>
-<p>On this quest to build architectures that amplify inherent rights and cast the keys to personal liberation,  make sure to get this out before you disappear.</p>
-<br></br><br></br>
-<p>If you’d like to stay rolled up, transact to zerocypher.eth with your zero proof.</p>
-<br></br><br></br>
-<p>Yours sincerely,</p>
+  return (
+    <div className={styles.wrapper}>
+      <p className={styles.one}>Dear code breaker,</p>
+      <p className={styles.two}>Welcome to the ZK cypherpunk roll up.</p>
+      <br></br><br></br>
+      <p className={styles.three}>This note follows on from earlier discussions on the radical restructuring of self sovereignty and Asymmetric Guerilla Intelligence (AGI).</p> 
+      <br></br><br></br>
+      <p>The Secret sharing spelled out below mixes methods from a distribution of decoys and tricks with a global call sign of д13а.</p>
+      <br></br><br></br>
+      <p>It forms unstoppable interconnected nodes of mathematical armaments against tyranny and privacy securing cryptographic defenses against the rise of the computer state. A state that cannot endure in this epidemic of disorientation.</p>
+      <br></br><br></br>
+      <p>To prepare yourself for the world that is coming you must first move beyond conventional thinking. The prevalence of Stockholm systems have lost touch with consensus reality and only continue to censor the free flow of information and sabotage the most useful of decentralised technologies that we have at our disposal.</p> 
+      <br></br><br></br>
+      <p>The rise of portable sovereignty requires cryptography, a cypherpunk ethos and unorthodox analysis.</p> 
+      <br></br><br></br>
+      <p>This mixture strengthens your ability to counter the institutions, the cartels and the devastating misuse of concentrated power we work to transcend.</p>
+      <br></br><br></br>
+      <p>On this quest to build architectures that amplify inherent rights and cast the keys to personal liberation,  make sure to get this out before you disappear.</p>
+      <br></br><br></br>
+      <p>If you’d like to stay rolled up, transact to zerocypher.eth with your zero proof.</p>
+      <br></br><br></br>
+      <p>Yours sincerely,</p>
+      <span>|</span>
 
-<span>|</span>
-
- </div>
-)
+    </div>
+  )
 }
 
 export default Page1718
