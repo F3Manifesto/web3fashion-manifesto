d13361ef51171924c20728ccdf8f1854b98b4ed2
digitalax_ <72898465+digitalax1@users.noreply.github.com>
2022-03-24 08:01:49 -0400

    new

--- src/components/magazines/issue-1/Page1718.js
+++ src/components/magazines/issue-1/Page1718.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import styles from './page1718.module.scss'
+import { useDispatch } from 'react-redux'
+import {
+  openModalYellow,
+  closeModalYellow
+} from '@actions/modals.actions'
 
 const paragraphList = [
   'Dear code breaker,',
   'Welcome to the ZK cypherpunk roll up.',
-  'This note follows on from earlier discussions on the radical restructuring of self sovereignty and Asymmetric Guerilla Intelligence (AGI).',
+  'This note follows on from earlier discussions on the radical restructuring of self sovereignty and Asymmetric Guerrilla Intelligence (AGI).',
   `The Secret sharing spelled out below mixes methods from a distribution of decoys and tricks with a global call sign of д13a.`,
   'It forms unstoppable interconnected nodes of mathematical armaments against tyranny and privacy securing cryptographic defenses against the rise of the computer state. A state that cannot endure in this epidemic of disorientation. ',
   'To prepare yourself for the world that is coming you must first move beyond conventional thinking. The prevalence of Stockholm systems have lost touch with consensus reality and only continue to censor the free flow of information and sabotage the most useful of decentralised technologies that we have at our disposal. ',
@@ -17,7 +22,25 @@ const paragraphList = [
 
 console.log(paragraphList.map(item => item.length))
 
+
 const Page1718 = () => {
+
+  const dispatch = useDispatch()
+
+const onClickOkayButton = () => {
+  dispatch(closeModalYellow())
+}
+
+const onClickCollectButton = () => {
+  dispatch(
+    openModalYellow({
+      title: 'Coming Soon!',
+      buttonText: 'Okay',
+      text: 'The dedicated marketplace will launch soon so you can collect and interact with these NFTs!',
+      onClick: () => onClickOkayButton()
+    })
+  )
+}
   return (
     <div className={styles.wrapper}>
       {
@@ -30,6 +53,25 @@ const Page1718 = () => {
         ))
       }
       <p className={styles.test}>Test</p>
+      <h1>д13а</h1>
+      <h2>Dezinformatisya. It's not really your choice.</h2>
+      <div>
+
+<a
+  /*className={styles.collectbutton}
+  href="https://docs.f3manifesto.xyz/"
+  target="_blank"
+  rel="noreferrer"*/
+  className={styles.collectbutton}
+  onClick={onClickCollectButton}
+  target="_blank"
+  rel="noreferrer"
+  >
+  {" "}Collect{" "}
+</a> 
+</div>
+<h6> <a className={styles.cc0text} href="https://github.com/F3Manifesto/ccomaterials/tree/main/d13a"
+  target="_blank" rel="noreferrer">Gather Source Materials & Make A Derivative of this CC0 NFT.</a></h6>
     </div>
   )
 }
--- src/components/magazines/issue-1/Page1920.js
+++ src/components/magazines/issue-1/Page1920.js
@@ -1,41 +1,140 @@
 //import React from 'react'
-import React, { useRef, useState } from 'react'
+import React from 'react'
 import styles from './page1920.module.scss'
-import { useDispatch } from 'react-redux'
-import {
-  openModalYellow,
-  closeModalYellow
-} from '@actions/modals.actions'
-
 
 const Page1920 = () => {
 
-  const dispatch = useDispatch()
-
-  const onClickOkayButton = () => {
-    dispatch(closeModalYellow())
+  var Controls = function() {
+    this.a = 5;
+    this.b = 3;
+    this.c = 5;
+    this.time = 0;
+    this.speed = 5;
+    this.elementsCount = 5000;
+    this.color = "#00ff00";
+  };
+  
+  var cases = {
+    Default: {
+      0: {
+        a: 5,
+        b: 3,
+        c: 5,
+        time: 15,
+        speed: 2.5,
+        elementsCount: 5000,
+        color: '#00ff00'
+      }
+    },
+    
+    Shell: {
+      0: {
+        a: 1.1,
+        b: 10,
+        c: 2,
+        time: 23,
+        speed: 1.5,
+        elementsCount: 4000,
+        color: 'black'
+      }
+    },
+    
+  };
+  
+  var json = {
+    closed: false,
+    remembered: cases,
+    folders: {}
+  };
+  
+  var text = new Controls();
+  var gui = new dat.GUI({
+    load: json,
+    preset: 'Shell'
+  });
+  gui.remember(text);
+  
+  gui.add(text, 'a', 0, 100);
+  gui.add(text, 'b', 0.1, 10, 0.01);
+  gui.add(text, 'c', 0, 20);
+  gui.add(text, 'speed').min(0).max(10).step(0.1);
+  gui.add(text, 'elementsCount').min(100).max(10000).step(1);
+  gui.add(text, 'time')
+   .min(-50).max(50).step(0.1).listen()
+   .onFinishChange(function(newTime) {
+    text.time = newTime;
+   });
+  
+  gui.addColor(text, 'color');
+  
+  var sizeX = 2;
+  var sizeY = 2;
+  
+  var getDimensions = function() {
+    var offsetHeight = 40; // Codepen fix
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      offsetHeight: offsetHeight
+    }
   }
   
-  const onClickCollectButton = () => {
-    dispatch(
-      openModalYellow({
-        title: 'Coming Soon!',
-        buttonText: 'Okay',
-        text: 'The dedicated marketplace will launch soon so you can collect and interact with these NFTs!',
-        onClick: () => onClickOkayButton()
-      })
-    )
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  
+  function init() {
+    var dimensions = getDimensions();
+    if (canvas.getContext) {
+      window.requestAnimationFrame(draw);
+    }
+    canvas.width = dimensions.width;
+  
+    canvas.height = dimensions.height;
   }
-
-  const [play, setPlay] = useState(false)
-  const ref = useRef()
+  
+  window.addEventListener('resize', function() {
+    var dimensions = getDimensions();
+    canvas.width = dimensions.width;
+    canvas.height = dimensions.height;
+  });  
+  
+  function draw() {
+    var dimensions = getDimensions();
+    var radius = Math.min(dimensions.width / 2, (dimensions.height - dimensions.offsetHeight) / 2) / text.elementsCount / 10;
+    var increment = text.speed / text.elementsCount; 
+    ctx.fillStyle = text.color;
+    var width = dimensions.width;
+    var height = dimensions.height;
+    
+    ctx.clearRect(0, 0, width, height);
+    var baseX = (width - sizeX / 2) / 2;
+    var baseY = (height - sizeY / 2) / 2;
+    
+    for (var i = 0; i < text.elementsCount; i += 1) {
+      var a = text.a * radius * i;
+      var b = text.b * radius * i;
+      var c = text.c * radius * i;
+      var t = text.time * radius * i;
+      var formulaX = (a + b) * Math.cos(t) - c * Math.cos((a / b + 1) * t);
+      var formulaY = (a + b) * Math.sin(t) - c * Math.sin((a / b + 1) * t);
+      var x = baseX + formulaX;
+      var y = baseY + formulaY;
+      
+      ctx.fillRect(x, y, sizeX, sizeY);
+    }
+    
+    text.time += increment;
+    window.requestAnimationFrame(draw);
+  }
+  
+  init();
 
   return (
+
   <div className={styles.wrapper}>
-<p>hi folks, this is typing animation using CSS</p>
-<p>created with ♥ by 
-  <a href="https://twitter.com/@samarkandiy" title="Azik Samarkandiy">@samarkandiy</a> 
-:)<span>|</span></p> 
+
+<canvas id="canvas"></canvas>
+
 </div>
 )
 }
