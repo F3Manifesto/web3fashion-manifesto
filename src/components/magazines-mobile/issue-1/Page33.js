3cc3be2d4a0b2559011eea8b85e4bad21d53a1ca
digitalax_ <72898465+digitalax1@users.noreply.github.com>
2022-04-24 07:15:28 -0400

    add time scene

--- src/components/magazines-mobile/issue-1/Page33.js
+++ src/components/magazines-mobile/issue-1/Page33.js
@@ -1,18 +1,68 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import styles from './page33.module.scss'
-
+import {
+  openModalYellow,
+  closeModalYellow
+} from '@actions/modals.actions'
 
 const Page33 = () => {
+  const dispatch = useDispatch()
+  const [play, setPlay] = useState(false)
+  const ref = useRef()
+
+  const onClickOkayButton = () => {
+    dispatch(closeModalYellow())
+  }
+  
+  const onClickCollectButton = () => {
+    dispatch(
+      openModalYellow({
+        title: 'Coming Soon!',
+        buttonText: 'Okay',
+        text: 'The dedicated marketplace will launch soon so you can collect and interact with these NFTs!',
+        onClick: () => onClickOkayButton()
+      })
+    )
+  }
 
 
   return (
     <div className={styles.wrapper}>
       <a href="https://polygonscan.com/tx/0x9acc61d00caeda0dfefffb75b4d89220ac68087b26585fc903c7e7d8069629b5" target="_blank" rel="noreferrer">
-      <img className={styles.nftblack} src='/images/nftblack.png'></img></a>
+      <img className={styles.nftblack} src='/images/nftwhite.png'></img></a>
 
-      <video playsInline autoPlay muted loop>
-      <source src='./magazine/1/videos/.mp4' type='video/mp4' />
-</video>
+      <video ref={ref} playsInline autoPlay loop muted> 
+      <source src='./magazine/1/videos/timescene.mp4' type='video/mp4' />
+    </video>
+    <img
+        src={`./magazine/1/1516/15_16_${!play ? 'play' : 'pause'}.png`}
+        className={styles.play}
+        onClick={() => {
+          setPlay(!play)
+          ref.current.muted = play
+        }}
+      />
+    <h4>All History Lost</h4>
+<div>
+
+<a
+  /*className={styles.collectbutton}
+  href="https://docs.f3manifesto.xyz/"
+  target="_blank"
+  rel="noreferrer"*/
+  className={styles.collectbutton}
+  onClick={onClickCollectButton}
+  target="_blank"
+  rel="noreferrer"
+  >
+  {" "}Collect{" "}
+</a> 
+</div>
+<h6> <a className={styles.cc0text} href="https://github.com/F3Manifesto/ccomaterials/tree/main/mypocketempire/allhistorylost"
+  target="_blank" rel="noreferrer">Gather Source Materials & Make A Derivative of this CC0 NFT.</a></h6>
+
+
  </div>
   )
 }
--- src/components/magazines/issue-1/Page3738.js
+++ src/components/magazines/issue-1/Page3738.js
@@ -1,32 +1,100 @@
-import React, {useRef} from 'react'
+//import React from 'react'
+import React, { useRef, useState } from 'react'
 import styles from './page3738.module.scss'
+import { useDispatch } from 'react-redux'
+import {
+  openModalYellow,
+  closeModalYellow,
+  openModalContent
+} from '@actions/modals.actions'
+
 
 const Page3738 = () => {
+
+  const dispatch = useDispatch()
+  const contentRef = useRef()
+
+
+  const onClickOkayButton = () => {
+    dispatch(closeModalYellow())
+  }
+  
+  const onClickCollectButton = () => {
+    dispatch(
+      openModalYellow({
+        title: 'Coming Soon!',
+        buttonText: 'Okay',
+        text: 'The dedicated marketplace will launch soon so you can collect and interact with these NFTs!',
+        onClick: () => onClickOkayButton()
+      })
+    )
+  }
+
+  const [play, setPlay] = useState(false)
+  const ref = useRef()
+
+  const onClickZoomOut = (isVideo, link) => {
+    dispatch(
+      openModalContent({
+        isVideo,
+        link
+      })
+    )
+  }
 
   return (
     <div className={styles.wrapper}>
       <video muted loop autoPlay className={styles.imageBackground}>
         <source
-          src='/magazine/1/videos/35scene.mp4'
+          src='/magazine/1/videos/timescene.mp4'
           type='video/mp4'
         />
       </video>
 
 
      <a href="https://polygonscan.com/tx/0x9acc61d00caeda0dfefffb75b4d89220ac68087b26585fc903c7e7d8069629b5" target="_blank" rel="noreferrer">
-    <img className={styles.nftwhite} src='/images/nftblack.png'></img></a>   
-      <h1>Freedom to Play</h1>
-      <h3><p>Are we really in charge of our own fate? 
-<br></br><br></br>
-Not when the gear we carry, places we go, memories we share, money we make, and even the stories we tell about ourselves are all owned and controlled by someone else. 
-<br></br><br></br>
-From pay to win, pay to play, and free to play to freedom to play. 
-<br></br><br></br>
-What is sovereignty in a massive multiplayer metaverse enabled world? 
-<br></br><br></br>
-To create or unlock your pocket empire, the clear conditions for freedom to play are dynamically set within portals to other free realities, markets, economies.
-<br></br><br></br>
-Liberate ownership beyond the hands of the mediocre few. Mod your way to a more seriously playful and radically self determined future.</p></h3>  
+    <img className={styles.nftwhite} src='/images/nftwhite.png'></img></a>   
+
+    <video ref={ref} autoPlay muted loop>
+      <source src='./magazine/1/videos/timescene.mp4' type='video/mp4' onClick={() => onClickZoomIn()}/>
+</video>
+<img
+        src={`./magazine/1/1516/15_16_${!play ? 'pause' : 'play'}.png`}
+        className={styles.play}
+        onClick={() => {
+          setPlay(!play)
+          ref.current.muted = play
+        }}
+      />
+
+<div
+        ref={contentRef}
+        
+        onClick={() => onClickZoomOut(true, './magazine/1/videos/timescene.mp4')}
+      >
+        <img className={styles.zoomButton} src="/images/expandwhite.png" />
+      </div>
+
+<h5 className={styles.h7}>All History Lost</h5>
+<div>
+
+<a 
+  /*className={styles.collectbutton}
+  href="https://docs.f3manifesto.xyz/"
+  target="_blank"
+  rel="noreferrer"*/
+  className={styles.collectbutton}
+  onClick={onClickCollectButton}
+  target="_blank"
+  rel="noreferrer"
+  >
+  {" "}Collect{" "}
+</a> 
+<p className={styles.h9}> <a className={styles.cc0text} href="https://github.com/F3Manifesto/ccomaterials/tree/main/mypocketempire/allhistorylost"
+  target="_blank" rel="noreferrer">Gather Source Materials & Make A Derivative of this CC0 NFT.</a></p>
+ </div>
+
+     
     </div>
     
   )
