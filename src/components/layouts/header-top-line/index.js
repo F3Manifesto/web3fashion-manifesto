a8494884db25fe3d81a8c42d3aa37b834635bc98
digitalax_ <72898465+digitalax1@users.noreply.github.com>
2022-05-13 19:58:50 -0400

    add header

--- src/components/layouts/header-top-line/index.js
+++ src/components/layouts/header-top-line/index.js
@@ -38,8 +38,8 @@ const HeaderTopLine = ({
         </button>
 
         <div className={styles.menuBar}>
-          <a href="https://docs.f3manifesto.xyz/" className={styles.link} target="_blank" rel="noreferrer">
-            DOCUMENTATION
+          <a href="https://f3manifesto.xyz/" className={styles.link} target="_blank" rel="noreferrer">
+            Home Port
           </a>
           <a href="https://web3fashionmanifesto.f3manifesto.xyz/manifesto/1/" className={styles.link} target="_blank" rel="noreferrer">
             Web3 Fashion manifesto
